e300b60c24f50e0b7c36da04f114ad4e9459b22d
Pronothurah <onsongojosphat28@gmail.com>
2024-05-27 15:46:04 +0300

    test_access_nested_map_exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -27,3 +27,17 @@ class TestAccessNestedMap(unittest.TestCase):
     ) -> None:
         """ Test `access_nested_map` method """
         self.assertEqual(access_nested_map(map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
+        """Tests `access_nested_map`'s exception raising."""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
